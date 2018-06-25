f9a7b7453081726979847de8db351f8092d34c38
luckycar <KuanHaoChen@chenguanhaode-macbook-air.local>
2018-06-25 14:37:33 +0800

    測試看看 Commit，加上了一個 sayHello() 的功能

--- GitExample/ViewController.swift
+++ GitExample/ViewController.swift
@@ -13,11 +13,16 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        sayHello()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func sayHello() {
+        print("Hello World!")
     }
 
 
