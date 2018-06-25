bc182976f7e8f0cb2b55129b5c0b89621de81241
luckycar <KuanHaoChen@chenguanhaode-macbook-air.local>
2018-06-25 14:44:55 +0800

    試試看新增一個檔案

---
+++ GitExample/TestClass.swift
@@ -0,0 +1,13 @@
+//
+//  TestClass.swift
+//  GitExample
+//
+//  Created by KuanHaoChen on 2018/6/25.
+//  Copyright © 2018年 KuanHaoChen. All rights reserved.
+//
+
+import UIKit
+
+class TestClass: NSObject {
+
+}
